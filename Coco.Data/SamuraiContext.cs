395cd594a4e0a6ce264bdd36461b2ce8f57db82b
Mohammad <m91.alahmadi@gmail.com>
2019-08-17 10:14:43 -0400

    bang bang

---
+++ Coco.Data/SamuraiContext.cs
@@ -0,0 +1,32 @@
+﻿using Coco.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace Coco.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        private static readonly ILoggerFactory _loggerFactory = new LoggerFactory()
+            .AddConsole((category, level) =>
+                level == LogLevel.Information, true);
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(_loggerFactory)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(
+                "Data Source=(localdb)\\ProjectsV13;Initial Catalog=Coco;Integrated Security=True;TrustServerCertificate=False;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>()
+                .HasKey(s => new { s.SamuraiId, s.BattleId });
+        }
+    }
+}
---
+++ Coco.UI/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Coco.Data;
+using Coco.Domain;
+
+namespace Coco.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InsertSamurai();
+            //InsertMultipleSamurais();
+            //SimpleSamuraiQuery();
+            MoreQueries();
+        }
+
+        private static void MoreQueries()
+        {
+            using (var context = new SamuraiContext())
+            {
+                var samurais = context.Samurais.Where(s => s.Name == "Sammy").ToList();
+            }
+        }
+
+        private static void SimpleSamuraiQuery()
+        {
+            using (var context = new SamuraiContext())
+            {
+                var samurais = context.Samurais.ToList();
+            }
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Julie" };
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.Add((samurai));
+                context.SaveChanges();
+            }
+        }
+
+        private static void InsertMultipleSamurais()
+        {
+            var samurai = new Samurai { Name = "Julie" };
+            var samuraiSammy = new Samurai { Name = "Sammy" };
+            using (var context = new SamuraiContext())
+            {
+                context.AddRange(samurai, samuraiSammy);
+                context.SaveChanges();
+            }
+        }
+    }
+}
